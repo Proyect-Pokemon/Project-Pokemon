178b078e26f187d1c1c83e03074ff57b51c95bab
Jesus Felipe Fuentes Trigueros <fuentestriguerosjesus@gmail.com>
2026-01-31 14:58:37 +0100

    Add MovementDataService and update movement loading
    Introduced MovementDataService to load movement data from the PokeAPI. Updated DataLoader to support loading movements, and registered MovementDataService in the DI container. Modified Movement entity to allow nullable Accuracy and Power, and removed default value configuration for Accuracy in the DbContext.

--- server/ProjectPokemon/ProjectPokemon/Services/Internal/DataLoader.cs
+++ server/ProjectPokemon/ProjectPokemon/Services/Internal/DataLoader.cs
@@ -8,17 +8,22 @@ public class DataLoader {
 
     private readonly PokemonDbContext _db;
     private readonly PokemonDataService _pokemonLoader;
+    private readonly MovementDataService _movementLoader;
     public DataLoader(
+    MovementDataService movementLoader,
     PokemonDbContext db,
     PokemonDataService pokemonLoader) {
         _db = db;
         _pokemonLoader = pokemonLoader;
+        _movementLoader = movementLoader;
     }
     public async Task LoadAllDataAsync() {
 
         await _pokemonLoader.LoadGenerationAsync(1);
 
         await LoadPokemons();
+
+        await LoadMovements();
     }
     private async Task LoadPokemons() {
         for (int id = 1; id <= 151; id++) {
@@ -29,6 +34,18 @@ public class DataLoader {
             await _db.SaveChangesAsync();
 
             Console.WriteLine($"Cargado Pokemon: {pokemon.Name}");
+        }
+    }
+
+    private async Task LoadMovements() {
+        for (int id = 1; id <= 165; id++) {
+            if (_db.Movements.Any(p => p.Id == id)) continue;
+
+            var moves = await _movementLoader.LoadMovement(id);
+            _db.Movements.Add(moves);
+            await _db.SaveChangesAsync();
+
+            Console.WriteLine($"Cargado Movimientos: {moves.Name}");
         }
     }
 }
