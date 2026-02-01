b231d139c11ba9e8d0a0331ea1de68fcfac89e09
Jor <jgonjim2511@g.educaand.es>
2026-02-01 21:32:03 +0100

    Add TeamsController with basic GET and POST endpoints
    Introduces TeamsController to provide API endpoints for retrieving and creating teams. Both endpoints currently return a default demo team with hardcoded data.

---
+++ server/ProjectPokemon/ProjectPokemon/Controllers/TeamsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ProjectPokemon.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeamsController : ControllerBase
+    {
+        // GET: api/teams
+        [HttpGet]
+        public IActionResult GetDefaultTeam()
+        {
+            var team = new List<object>
+            {
+                new
+                {
+                    name = "Equipo Demo",
+                    pokemons = new List<string> { "Charmander", "Pidgey" }
+                }
+            };
+
+            return Ok(team);
+        }
+
+        // POST: api/teams
+        [HttpPost]
+        public IActionResult CreateTeam([FromBody] object teamRequest)
+        {
+            // Aquí podemos simplemente devolver siempre el equipo por defecto
+            var defaultTeam = new
+            {
+                name = "Equipo Demo",
+                pokemons = new List<string> { "Charmander", "Pidgey" }
+            };
+
+            return Ok(defaultTeam);
+        }
+    }
+}
