8117e7a8b005d7a8e40322f62f60d69967dfab39
Jor <jgonjim2511@g.educaand.es>
2026-03-03 12:35:43 +0100

    Update UsersController.cs
    Copiado, pegado y adaptado del otro proyecto
    
    - Eliminado endpoint POST para alta de usuarios
    - Eliminada dependencia de PasswordHelper
    - Eliminada exposición del campo Password en el listado de usuarios
    - Añadido AvatarPath en el DTO de respuesta del GET
    - Controlador simplificado a funcionalidad únicamente de consulta
    
    Se mejora la seguridad evitando exponer hashes de contraseña y se restringe la gestión de usuarios desde este controlador.

---
+++ server/ProjectPokemon/ProjectPokemon/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectPokemon.Models.Database;
+using ProjectPokemon.Models.Database.Entities;
+using ProjectPokemon.Models.Dtos.User;
+
+namespace ProjectPokemon.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase {
+    // Inyeccion de UserRepository 
+    private readonly UnitOfWork _unitOfWork;
+
+    public UsersController(UnitOfWork unitOfWork) {
+        _unitOfWork = unitOfWork;
+    }
+
+    //GET: api/users
+    [HttpGet]
+    public async Task<IEnumerable<GetUserDto>> GetAllUsers() {
+        ICollection<User> users = await _unitOfWork.UserRepository.GetAllAsync();
+
+        IEnumerable<GetUserDto> getUsersDto = users.Select(user => new GetUserDto {
+            Id = user.Id,
+            Nickname = user.Nickname,
+            AvatarPath = user.AvatarPath!
+        });
+        return getUsersDto;
+    }
+}
