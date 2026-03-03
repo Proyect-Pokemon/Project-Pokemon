164f469361e90a49b4f9d73e045c71c43a2a6858
Jor <jgonjim2511@g.educaand.es>
2026-03-03 18:19:17 +0100

    Eliminar password de GetUserDto y añadir AvatarPath

--- server/ProjectPokemon/ProjectPokemon/Models/Dtos/User/GetUserDto.cs
+++ server/ProjectPokemon/ProjectPokemon/Models/Dtos/User/GetUserDto.cs
@@ -2,6 +2,6 @@
     public class GetUserDto {
         public long Id { get; set; }
         public required string Nickname { get; set; }
-        public required string Password { get; set; }
+        public string AvatarPath { get; set; } = "/defaultAvatar.png"; //TO DO: en vez de imagen por defecto, hay que poner null y qie si es null añadirle x imagen
     }
 }
