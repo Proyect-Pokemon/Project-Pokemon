49d9df631dc28d437d51cb04ec13cece21446571
Jesus Felipe Fuentes Trigueros <235039123+jffuentes-15@users.noreply.github.com>
2026-03-06 18:55:28 +0100

    Update Movement.cs

--- server/ProjectPokemon/ProjectPokemon/Models/Database/Entities/Movement.cs
+++ server/ProjectPokemon/ProjectPokemon/Models/Database/Entities/Movement.cs
@@ -11,7 +11,7 @@ public class Movement {
     public int? Power { get; set; } = null; // Potencia del movimiento. Un movimiento de clase estado tendría 0
     public required PokeTarget Target { get; set; }
     public required int Priority { get; set; } // <-- Consultar el documento con las prioridades en combate
-    public required int EffectChance { get; set; } // Probabilidad que tiene el movimiento de que ocurra el efecto secundario. Es el num en %
+    public int? EffectChance { get; set; } // Probabilidad que tiene el movimiento de que ocurra el efecto secundario. Es el num en %
     public required PokeType Type { get; set; }
 
     // Relaciones
