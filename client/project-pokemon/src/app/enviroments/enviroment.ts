cd739ff8077235020ce816988f774114562ba325
Jor <202547325+jor2511@users.noreply.github.com>
2026-06-10 21:32:08 +0200

    Secret Dummy Values

--- client/project-pokemon/src/app/enviroments/enviroment.ts
+++ client/project-pokemon/src/app/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   apiUrl: 'https://projectpokemon.runasp.net/api/',
   wsUrl: 'wss://projectpokemon.runasp.net/ws',
-  GOOGLE_CLIENT_ID: ''
+  GOOGLE_CLIENT_ID: 'dummy-google-client-id.apps.googleusercontent.com'
 };
