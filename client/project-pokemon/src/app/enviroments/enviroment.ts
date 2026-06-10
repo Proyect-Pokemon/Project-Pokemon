2e2467a3f7b7c85a4a7f57cf351e5a6efd7d2ed5
Jor <202547325+jor2511@users.noreply.github.com>
2026-06-10 21:48:41 +0200

    Swagger Usable

--- client/project-pokemon/src/app/enviroments/enviroment.ts
+++ client/project-pokemon/src/app/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: 'https://projectpokemon.runasp.net/api/',
-  wsUrl: 'wss://projectpokemon.runasp.net/ws',
+  apiUrl: 'https://localhost:7277/api/',
+  wsUrl: 'wss://https://localhost:7277/ws',
   GOOGLE_CLIENT_ID: 'dummy-google-client-id.apps.googleusercontent.com'
 };
